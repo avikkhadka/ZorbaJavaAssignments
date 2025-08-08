90c05db56fc71992103517c199bb85b5c3eb740f
Avik Khadka <avikkhadka10@gmail.com>
2025-08-08 12:54:04 -0400

    test class updated

--- src/Test.java
+++ src/Test.java
@@ -1,23 +1,19 @@
 public class Test {
     public static void main(String[] args) {
+        int N = 121;
+        int D = 1;
+        int count = 0;
 
+        int number = N;
 
-        int num1=25;
-        int num2=39;
-
-        if(num1!=num2)
-        {
-            System.out.println(num1+"!="+num2);
-        }
-        if(num1<num2)
-        {
-            System.out.println(num1+"<"+num2);
-        }
-        if(num1<=num2)
-        {
-            System.out.println(num1+"<="+num2);
+        while (number > 0) {
+            int lastDigit = number % 10;
+            if (lastDigit == D) {
+                count++;
+            }
+            number = number / 10;
         }
 
-
+        System.out.println("Digit " + D + " occurs " + count + " times in number " + N);
     }
 }
