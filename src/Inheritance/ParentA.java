9f8b04aea175e4fbd717ad1451bf48203578c003
Avik Khadka <avikkhadka10@gmail.com>
2025-08-08 11:31:42 -0400

    is a relationship done

---
+++ src/Inheritance/ParentA.java
@@ -0,0 +1,21 @@
+package Inheritance;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class ParentA {
+    public static void main(String[] args) {
+        ChildB childBObject=new ChildB();
+        ParentA parentAObject=new ParentA();
+        ParentA parentAObject1=new ChildB();
+        System.out.println("Checking if parent object is an instance of child: "+(parentAObject1 instanceof ChildB));
+        System.out.println("Checking if parent object is an instance of parent class: "+(parentAObject1 instanceof ParentA));
+    }
+
+
+}
+class ChildB extends ParentA{
+
+
+
+
+}
